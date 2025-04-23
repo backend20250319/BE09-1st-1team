d3827db82c5bf63abd8d1ab1c49e5b541e64718f
Seokjin Lee <seokjin.dev@gmail.com>
2025-04-23 17:11:50 +0900

    MainMenuView

--- src/main/java/com/bookmark/library/Main.java
+++ src/main/java/com/bookmark/library/Main.java
@@ -1,13 +1,13 @@
 package com.bookmark.library;
 
+import com.bookmark.library.view.MainMenuView;
+
 public class Main {
     public static void main(String[] args) {
-        String version = System.getProperty("java.version");
-        String runtimeVersion = System.getProperty("java.runtime.version");
-        String vendor = System.getProperty("java.vendor");
-
-        System.out.println("Java Version: " + version);
-        System.out.println("Java Runtime Version: " + runtimeVersion);
-        System.out.println("Java Vendor: " + vendor);
+        var mainMenu = new MainMenuView();
+        boolean terminate = false;
+        while (!terminate) {
+            terminate = !mainMenu.showMainMenu();
+        }
     }
 }
---
+++ src/main/java/com/bookmark/library/view/MainMenuView.java
@@ -0,0 +1,51 @@
+package com.bookmark.library.view;
+
+import com.bookmark.library.util.IO;
+
+public class MainMenuView {
+    /**
+     * 메인 메뉴에 진입합니다.
+     * @return 프로그램 종료가 요청되면 false를 반환합니다. 그 외의 경우 true를 반환합니다.
+     */
+    public boolean showMainMenu() {
+        System.out.print("""
+                
+                ==== BOOKMARK ====
+                도서 대출 관리 시스템
+                
+                1. 회원가입
+                2. 로그인
+                3. 통합검색
+                4. 카테고리 전체보기
+                0. 프로그램 종료
+                """);
+
+        switch (IO.selectMenu(4)) {
+            case 0 -> {
+                return false;
+            }
+            case 1 -> registerMember();
+            case 2 -> login();
+            case 3 -> searchBook();
+            case 4 -> viewCategories();
+        }
+
+        return true;
+    }
+
+    private void registerMember() {
+
+    }
+
+    private void login() {
+
+    }
+
+    private void searchBook() {
+
+    }
+
+    private void viewCategories() {
+
+    }
+}
