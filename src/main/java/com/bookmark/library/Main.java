c9d6a42aa23a014fae136d239571b2f38f670bff
Seokjin Lee <seokjin.dev@gmail.com>
2025-04-23 15:02:36 +0900

    Change SDK version

--- src/main/java/com/bookmark/library/Main.java
+++ src/main/java/com/bookmark/library/Main.java
@@ -2,6 +2,12 @@ package com.bookmark.library;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String version = System.getProperty("java.version");
+        String runtimeVersion = System.getProperty("java.runtime.version");
+        String vendor = System.getProperty("java.vendor");
+
+        System.out.println("Java Version: " + version);
+        System.out.println("Java Runtime Version: " + runtimeVersion);
+        System.out.println("Java Vendor: " + vendor);
     }
 }
