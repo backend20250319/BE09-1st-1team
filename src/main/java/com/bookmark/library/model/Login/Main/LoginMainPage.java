c6cf10739e2f3c6c23668b5d685e2ac721d3253e
junbem <junbem0728@naver.com>
2025-04-23 20:26:28 +0900

    로그인 페이지 구현

---
+++ src/main/java/com/bookmark/library/model/Login/Main/LoginMainPage.java
@@ -0,0 +1,23 @@
+package com.bookmark.library.model.Login.Main;
+
+import com.bookmark.library.model.Login.View.LoginFalse;
+import com.bookmark.library.model.Login.service.LoginService;
+import com.bookmark.library.util.IO;
+
+public class LoginMainPage {
+    public static void main(String[] args) {
+        LoginService service = new LoginService();
+
+        System.out.print("아이디를 입력하세요: ");
+        String username = IO.scanner.nextLine();
+
+        System.out.print("비밀번호를 입력하세요: ");
+        String password = IO.scanner.nextLine();
+
+        if (service.login(username, password)) {
+            System.out.println("✅ 로그인 성공!");
+        } else {
+            LoginFalse.display();   // 로그인 실패 시 로그인 실패 페이지로 이동
+        }
+    }
+}
---
+++ src/main/java/com/bookmark/library/model/Login/View/LoginFalse.java
@@ -0,0 +1,7 @@
+package com.bookmark.library.model.Login.View;
+
+public class LoginFalse {
+    public static void display() {
+        System.out.println("❌ 로그인 실패. 다시 시도해주세요.");
+    }
+}
