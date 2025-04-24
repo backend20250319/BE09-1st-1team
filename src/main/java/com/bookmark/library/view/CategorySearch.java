a6747a4bc34c6527a9b7096adee2b3c151a55743
BeomSeok <abcdef980530@naver.com>
2025-04-24 10:08:38 +0900

    1

---
+++ src/main/java/com/bookmark/library/view/CategorySearch.java
@@ -0,0 +1,35 @@
+package com.bookmark.library.view;
+
+import com.bookmark.library.model.Book;
+import com.bookmark.library.util.IO;
+
+import java.util.Set;
+
+public class CategorySearch {
+
+    private final Book book1 = new book1();
+
+    public boolean showCategoryView() {
+        System.out.println("""
+                 ==== [도서 카테고리 선택] ====
+
+                 1. 문학
+                 2. 과학 및 기술
+                 3. 취미·예술·생활
+                 4. 자기계발·비즈니스
+                 0. 뒤로가기
+                """);
+
+        int category_id = IO.selectMenu(4);
+
+        if (category_id == 0) return false;
+        if (category_id < 1 || category_id > 4) {
+            System.out.println("잘못된 입력입니다.");
+            return true;
+        }
+
+        Set<String> results = book1.getBooksByCategory(category_id);
+        SearchView.setCategoryResults(results);
+        return true;
+    }
+}
---
+++ src/main/java/com/bookmark/library/view/SearchView.java
@@ -0,0 +1,79 @@
+package com.bookmark.library.view;
+import com.bookmark.library.util.IO;
+
+
+import java.util.Set;
+public class SearchView {
+    public static Set<String> searchResults;
+    private static String books;
+
+    private static void showSearchResults() {
+        System.out.println("==== [검색 결과] ====");
+        if (searchResults == null || searchResults.isEmpty()) {
+            System.out.println("검색 결과가 없습니다.");
+            return;
+        }
+
+        int index = 1;
+        for (String result : searchResults) {
+            System.out.println("[" + index + "] " + result);
+            index++;
+        }
+
+        System.out.println("0. 홈으로");
+        System.out.println("1. 책 상세 정보");
+        switch (IO.selectMenu(1)){
+            case 0 -> {} //처음으로
+            case 1 -> {} //상세정보
+            default -> {
+                System.out.println("잘못된 입력입니다.");
+                return ;
+            }
+    }
+    }
+
+
+    public static Set<String> categoryResults;
+
+    public static void setCategoryResults(Set<String> results) {
+        categoryResults = results;
+        showCategoryResults();
+    }
+
+    private static void showCategoryResults() {
+        System.out.println("==== [카테고리 검색 결과] ====");
+
+        if (categoryResults == null || categoryResults.isEmpty()) {
+            System.out.println("검색 결과가 없습니다.");
+            System.out.println("===========================");
+            return;
+        }
+
+        int index = 1;
+        for (String book : categoryResults) {
+            System.out.println("[" + index++ + "] " + book);
+        }
+
+        System.out.println("""
+                
+                0. 메인화면으로 이동
+                1. 상세페이지로 이동
+                """);
+
+        switch (IO.selectMenu(1)) {
+            case 0 -> {
+                System.out.println("메인화면으로 이동합니다.");
+                // 메인 메뉴로 이동
+            }
+            case 1 -> {
+                System.out.println("상세페이지로 이동합니다.");
+                // 상세 페이지 이동
+            }
+            default -> {
+                System.out.println("잘못된 입력입니다.");
+            }
+        }
+
+        System.out.println("===========================");
+    }
+}
