8f3f9a8fc16f96790de6a28554fdfb23862edbba
junbem <junbem0728@naver.com>
2025-04-24 11:51:30 +0900

    아이디, 비번 동시에 가져와서 조회

--- src/main/java/com/bookmark/library/auth/LoginContext.java
+++ src/main/java/com/bookmark/library/auth/LoginContext.java
@@ -11,16 +11,21 @@ public class LoginContext {
 
     public static boolean login(String memberId, String password) {
         boolean isValid = MemberDAO.validateLogin(memberId, password);
+        if (isValid) {
+            loginContext.memberId = memberId;
+            loginContext.password = password; // 비밀번호도 저장
+        }
         return isValid;
     }
 
     // 현재 로그인한 사용자 정보 가져오기
     public static Member getCurrentUser() {
         if (isLoggedIn()) {
-            return MemberDAO.getUserInfo(loginContext.memberId); // DAO에서 회원 정보 조회
+            return (Member) MemberDAO.getUserInfo(loginContext.memberId, loginContext.password); // memberId + password로 조회
         }
         return null;
     }
+
 
 
     // 로그인 여부 확인
